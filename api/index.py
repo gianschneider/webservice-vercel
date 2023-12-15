9e24a4ba9c9f6e0026d4c83ba6ba4a74c8898af6
Gian Schneider <gianschneider12@bluewin.ch>
2023-12-15 18:37:46 +0100

    abgabe uebung11

--- api/index.py
+++ api/index.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, Request
+from pyproj import Transformer
 
 app = FastAPI()
+lv95 = "EPSG:2056"
+wgs84 = "EPSG:4326"
 
 @app.middleware("http")
 async def addcors(request: Request, call_next):
@@ -23,3 +26,9 @@ def liste():
 @app.get("/api/add")
 async def read_item(a: int = 0, b: int = 0):
     return {"sum": a+b}
+
+@app.get("/api/transformiere")
+async def transformiere(E: float =0, N: float =0):
+    t2 = Transformer.from_crs(lv95, wgs84)
+    return {"KoordinatenWGS84": t2.transform(E, N)
+    }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,36 @@
-import React, {useState, useEffect} from 'react';
-import {AppBar, Toolbar, Button} from '@mui/material';
-import FruchtListe from './fruchtliste';
+import React, {useState, useEffect} from "react";
+import {AppBar, Toolbar,Button} from "@mui/material"
+import FruchtListe from "./fruchtliste"
+import Addiere from "./addiere"
+import Transformation from "./transformation"
 
-export default function Index() {
-    const [tab, setTab] = useState('tab1');
+
+export default function Index(){
+    const[tab, setTab] = useState("tab1");
 
     return (<>
     <AppBar position="static">
         <Toolbar>
-            <Button color="inherit" onClick={() => setTab('tab1')}>
-                Tab 1
+            <Button color = "inherit" onClick={() => setTab("tab1")}>
+                Home
             </Button>
-            <Button color="inherit" onClick={() => setTab('tab2')}>
-                Tab 2
+            <Button color = "inherit" onClick={() => setTab("tab2")}>
+                Früchte
+            </Button>
+            <Button color = "inherit" onClick={() => setTab("tab3")}>
+                Addiere
+            </Button>
+            <Button color = "inherit" onClick={() => setTab("tab4")}>
+                Koordinatentransformation
             </Button>
         </Toolbar>
     </AppBar>
 
-    {tab === 'tab1' && (<><h1>Inhalt Tab 1</h1></> )}
-    {tab === 'tab2' && (<><h1><FruchtListe /></h1></> )}
+    {tab === "tab1" && (<><h1>Inhalt Tab 1</h1></>)}
+    {tab === "tab2" && (<FruchtListe/>)}
+    {tab === "tab3" && (<Addiere/>)}
+    {tab === "tab4" && (<Transformation/>)}
+
 
     </>)
 }
